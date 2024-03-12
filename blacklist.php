0aa2ddb8f77e328eac283b1300b578595a16324c
Eduard Gabriel <50186245+EduardGabriel@users.noreply.github.com>
2024-03-12 01:24:15 +0000

    Update blacklist.php

--- blacklist.php
+++ blacklist.php
@@ -1,5 +1,5 @@
 <?php
-$deny = array("109.98.132.224","79.117.203.75");
+$deny = array("");
 if (in_array ($_SERVER['REMOTE_ADDR'], $deny)) {
    header("location: https://www.edward1337.xyz/404/");
    echo("BLACKLISTED IP ADDRESS");
